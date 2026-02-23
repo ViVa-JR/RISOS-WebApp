21bb553078a17ff29e772e8308ca7a0d20521d54
SafarVitek <vitezslav.safar@znojmo.cz>
2026-02-24 00:26:33 +0100

    feat: Added lists of subjects and loading into the dropzones

--- src/RISOS/Pages/Home/Models/SubjectEntry.cs
+++ src/RISOS/Pages/Home/Models/SubjectEntry.cs
@@ -1,3 +1,4 @@
+using RISOS.Enums;
 using RISOS.Models;
 
 namespace RISOS.Pages.Home.Models;
@@ -5,7 +6,7 @@ namespace RISOS.Pages.Home.Models;
 public class SubjectEntry(Subject subject)
 {
     public Subject Subject { get; } = subject;
-    public string Semester { get; set; } = Unassigned;
+    public string Semester { get; set; } = subject.Type == SubjectType.Compulsory ? $"{subject.MinSemester}" : Unassigned;
     
     public bool IsAssigned => Semester != Unassigned;
     
