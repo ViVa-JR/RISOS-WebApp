881ff840862e412304373707e0a9a0ed9bd03b69
SafarVitek <vitezslav.safar@znojmo.cz>
2026-03-15 18:35:12 +0100

    feat: Fixed clearing imported file

--- src/RISOS/Pages/Home/Dialogs/AddCustomSubjectDialog/AddCustomSubjectModel.cs
+++ src/RISOS/Pages/Home/Dialogs/AddCustomSubjectDialog/AddCustomSubjectModel.cs
@@ -14,9 +14,12 @@ public class AddCustomSubjectModel
 
     public SemesterSeason SemesterSeason { get; set; } = SemesterSeason.Any;
     public SubjectType Type { get; set; } = SubjectType.Compulsory;
-    
+
     public string? GroupId { get; set; } = null;
 
-    public SubjectEntry ToSubjectEntry() => new (new Subject("custom-" + Name + ShortName, Name, ShortName,  Credits, MinSemester, Type, SemesterSeason, CompletionType, GroupId)) 
-    { Semester = SubjectEntry.Unassigned};
+    public SubjectEntry ToSubjectEntry()
+        => new(new Subject("custom-" + Name + ShortName, Name, ShortName, Credits, MinSemester, Type, SemesterSeason, CompletionType, GroupId))
+        {
+            Semester = SubjectEntry.Unassigned
+        };
 }
