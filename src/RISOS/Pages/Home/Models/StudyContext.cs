1a1e50f40472a726d727090c263964ad94c6d4e1
SafarVitek <vitezslav.safar@znojmo.cz>
2026-05-03 14:16:16 +0200

    feat: Fixed mobile version and added displaying Faculty, study program and specialization

--- src/RISOS/Pages/Home/Models/StudyContext.cs
+++ src/RISOS/Pages/Home/Models/StudyContext.cs
@@ -2,6 +2,9 @@ namespace RISOS.Pages.Home.Models;
 
 public class StudyContext
 {
+    public string? ProgramAbbreviation { get; set; }
+    public string? ProgramSpecialization { get; set; }
+    public string? FacultyAbbreviation { get; set; }
     public int RequiredCredits { get; set; } = 180;
     public int? CreditOverride { get; set; }
     public int Years { get; set; } = 3;
